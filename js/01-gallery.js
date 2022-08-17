79c3937410319da07c99449d9ccf84a2d0353de6
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-08-18 01:14:20 +0300

    small-change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -65,16 +65,16 @@ function onImgClick(e) {
 // Разметка элемента галереи
 // Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
 
-{/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */}
+// {/* <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div> */}
 // Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
 
 // Закрытие с клавиатуры
